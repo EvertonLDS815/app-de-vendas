8bbc194c5155184eb74ad37132d9df0caa88ebbb
EvertonLDS815 <user.name>
2022-12-19 16:06:37 -0300

    Alterando o Price para Number

--- src/app/models/Product.ts
+++ src/app/models/Product.ts
@@ -13,7 +13,7 @@ export const Product = model('Product', new Schema({
         required: true,
     },
     price: {
-        type: String,
+        type: Number,
         required: true,
     },
     ingredients: {
